b228893e385504b781e986aaf78f1c233880f8ef
Chaosholz <chaosholz.0@gmail.com>
2026-01-07 13:33:28 +0100

    SyncPatch Update

--- LateRepo/Patches/PlayerJoinSyncPatch.cs
+++ LateRepo/Patches/PlayerJoinSyncPatch.cs
@@ -1,15 +1,54 @@
 ﻿using HarmonyLib;
+using Photon.Pun;
 using Photon.Realtime;
+using System;
+using System.Collections;
+using UnityEngine;
 
 
 namespace LateRepo.Patches {
-    
+
     [HarmonyPatch(typeof(NetworkManager))]
     internal class PlayerJoinSyncPatch {
-        [HarmonyPatch(typeof(NetworkManager), "OnPlayerEnteredRoom")]
-        [HarmonyPrefix]
-        public static void OnPlayerEnteredRoomPatch(Player newPlayer) {
 
+        // Variablen //
+        private const string FieldLoadingDone = "loadingDone";
+        private const string RpcALlPlayersSpawned = "AllPlayerSpawnedRPC";
+
+        [HarmonyPatch(typeof(NetworkManager), "OnPlayerEnteredRoom")]
+        [HarmonyPostfix]
+        public static void OnPlayerEnteredRoomPatch(NetworkManager __instance, Player newPlayer) {
+            try {
+                if (!PhotonNetwork.IsMasterClient) {
+                    return;
+                }
+                var loadingDOneField = AccessTools.Field(typeof(NetworkManager), FieldLoadingDone);
+                if (loadingDOneField != null) {
+                    loadingDOneField.SetValue(__instance, false);
+                }
+                __instance.StartCoroutine(ResendAllPlayerSpawnDelayed(__instance, 1.0f));
+
+            } catch (Exception e) {
+                Plugin.logger.LogError($"[LateJoinSync] Error in OnPlayerEnteredRoomPatch {e}");
+            }
+        }
+
+        private static IEnumerator ResendAllPlayerSpawnDelayed(NetworkManager nm, float delaySeconds) {
+            yield return new WaitForSeconds(delaySeconds);
+            if (nm = null) {
+                yield break;
+            }
+            if (!PhotonNetwork.IsMasterClient) {
+                yield break ;
+            }
+            PhotonView pv = nm.GetComponent<PhotonView>();
+            if (pv == null) {
+                yield break;
+            }
+
+            pv.RPC(RpcALlPlayersSpawned, RpcTarget.AllBuffered, []);
+
+            Plugin.logger.LogDebug("Resent AllPlayersSpawnedRPC");
         }
     }
 }
